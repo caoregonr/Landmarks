9a11a1995127e716d8e6af93281403d98555a1fa
Camila Oregon <ca.oregonr@gmail.com>
2020-02-28 19:58:57 -0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Camila Oregon Reyes on 28-02-20.
+//  Copyright © 2020 Camila Oregon Reyes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
